312139a5120d7ae032a8d1fed3139b00a12b333c
Hedrian Kuckert <hedrian.kuckert@gmail.com>
2023-09-07 17:57:31 -0300

    fase final

--- projeto_cad_usuarios/app_cad_usuarios/views.py
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+from .models import Usuario
+
 def home(request):
     return render(request,'usuarios/home.html')
-# Create your views here.
+
+def usuarios(request):
+    # Salvar os dados da tela para o banco de dados.
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+    # Exibir todos os usuários ja cadastrados em um nova página
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+    # retornar os dados para a página de usuários.
+    return render(request,'usuarios/usuarios.html',usuarios)
